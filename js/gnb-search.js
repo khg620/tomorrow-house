94e01f496491300f311b539a34ff2a147ed5e9f3
khg620 <rdchi3@naver.com>
2022-04-29 00:07:04 +0900

    🚧 Implement deleting all search-history items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearch.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,6 +15,11 @@ function closeGnbSearchHistoryOnClickingOutside(e) {
 }
 
 function openGnbSearchHistory() {
+  //★ 최근 검색어가 없을 경우 search-history는 나타나지 않는다 =>
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
@@ -17,3 +27,11 @@ function openGnbSearchHistory() {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistoryItems() {
+  //★ 자식요소 모두 지우는 방법
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
